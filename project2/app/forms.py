08a8cf25840faf6d101ca565243574a2be8e5963
fangyang <fangyang1994@gmail.com>
2017-04-12 23:15:47 -0400

    Override functions in the icrawler module

--- project2/app/forms.py
+++ project2/app/forms.py
@@ -4,6 +4,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Length, EqualTo
 from instaLooter import InstaLooter
+from six.moves.urllib.parse import urlparse
+import icrawler
 from icrawler.builtin import GoogleImageCrawler
 from icrawler.builtin import GreedyImageCrawler
 from boto3.dynamodb.conditions import Key
@@ -18,6 +20,86 @@ from app import webapp
 
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+file_names = []
+
+'''
+
+We Override the function in icrawler
+
+'''
+
+def new_get_filename(self, task, default_ext):
+    """Set the path where the image will be saved.
+
+    The default strategy is to use an increasing 6-digit number as
+    the filename. You can override this method if you want to set custom
+    naming rules. The file extension is kept if it can be obtained from
+    the url, otherwise ``default_ext`` is used as extension.
+
+    Args:
+        task (dict): The task dict got from ``task_queue``.
+
+    Output:
+        Filename with extension.
+    """
+    url_path = urlparse(task['file_url'])[2]
+    new_url_path = ""
+    extension = url_path.split('.')[-1] if '.' in url_path else default_ext
+    for ch in url_path:
+        if ch == '/':
+            ch = '_'
+        new_url_path += ch
+    return new_url_path
+
+icrawler.Downloader.get_filename = new_get_filename
+
+def new_download(self, task, default_ext, timeout=5, max_retry=3, **kwargs):
+    """Download the image and save it to the corresponding path.
+
+    Args:
+        task (dict): The task dict got from ``task_queue``.
+        timeout (int): Timeout of making requests for downloading images.
+        max_retry (int): the max retry times if the request fails.
+        **kwargs: reserved arguments for overriding.
+    """
+
+    file_url = task['file_url']
+
+    retry = max_retry
+    while retry > 0 and not self.signal.get('reach_max_num'):
+        try:
+            response = self.session.get(file_url, timeout=timeout)
+        except Exception as e:
+            self.logger.error('Exception caught when downloading file %s, '
+                              'error: %s, remaining retry times: %d',
+                              file_url, e, retry - 1)
+        else:
+            if self.reach_max_num():
+                self.signal.set(reach_max_num=True)
+                break
+            elif response.status_code != 200:
+                self.logger.error('Response status code %d, file %s',
+                                  response.status_code, file_url)
+                break
+            elif not self.keep_file(response, **kwargs):
+                break
+            with self.lock:
+                self.fetched_num += 1
+                filename = self.get_filename(task, default_ext)
+            self.logger.info('image #%s\t%s', self.fetched_num, file_url)
+            # self.storage.write(filename, response.content)
+            s3 = boto3.client('s3')
+            fp = io.BytesIO(urlopen(file_url).read())
+
+            s3.upload_fileobj(fp, 'ece1779project', filename)
+
+            file_names.append(filename)
+            break
+        finally:
+            retry -= 1
+
+icrawler.Downloader.download = new_download
 
 '''
 
@@ -129,13 +211,21 @@ def image_crawler():
     num = request.form.get('num')
     num = int(num)
     radio = request.form.get('gridRadios')
-    print (radio)
 
-    if radio == '500px':
-        url = 'https://500px.com/search?submit=Submit&q={}&type=photos'.format(target)
+    if radio == 'Greedy':
+        url = target
+        url=str(url)
         greedy_crawler = GreedyImageCrawler(storage={'root_dir': 'downloaded_pictures'})
         greedy_crawler.crawl(domains=url, max_num=num,
-                             min_size=None, max_size=None)
+                             min_size=(200, 200), max_size=None)
+        print(file_names)
+        for file_name in file_names:
+            response = table.put_item(
+                Item={
+                    'username': session['username'],
+                    'imagename': file_name,
+                }
+            )
 
     if radio == 'Instagram':
         looter = InstaLooter(directory="./downloaded_pictures", profile=target)
@@ -167,5 +257,12 @@ def image_crawler():
         google_crawler.crawl(keyword=target, max_num=num,
                              date_min=None, date_max=None,
                              min_size=(200, 200), max_size=None)
+        for file_name in file_names:
+            response = table.put_item(
+                Item={
+                    'username': session['username'],
+                    'imagename': file_name,
+                }
+            )
 
     return render_template("/imagecrawler/form.html")
