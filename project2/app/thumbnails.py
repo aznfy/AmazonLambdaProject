ca391362ff33d524e9f5e41d45b1f1e15c42d018
fangyang <fangyang1994@gmail.com>
2017-04-21 14:58:40 -0400

    no message

--- project2/app/thumbnails.py
+++ project2/app/thumbnails.py
@@ -1,6 +1,13 @@
-from flask import Flask, request, render_template, redirect, url_for, flash, g
+from flask import Flask, request, render_template, redirect, url_for, flash, g, send_file
 import boto3
+import os
 from app import webapp
+import urllib2
+import urllib
+import StringIO
+from io import BytesIO
+from io import FileIO
+import zipfile
 from boto3.dynamodb.conditions import Key
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
@@ -25,29 +32,60 @@ def thumb_list(username):
         #print (urls)
     return render_template('/thumbnails/list.html', urls=urls, username=username)
 
+@webapp.route('/download/<username>',methods=['GET'])
+def download(username):
+    s3 = boto3.client('s3')
+    table = dynamodb.Table('Images')
+    response = table.query(
+        KeyConditionExpression=Key('username').eq(username)
+    )
+    print(response)
+    urls = []
+    # memory_file = FileIO()
+    # zf = zipfile.ZipFile(memory_file, mode='a')
+    for i in response['Items']:
+        url = s3.generate_presigned_url(
+            ClientMethod='get_object',
+            Params={
+                'Bucket': 'ece1779project',
+                'Key': i['imagename']
+            }
+        )
+        req = urllib2.Request(url)
+        img = StringIO.StringIO(urllib2.urlopen(req).read())
+        # zf.write(img, arcname=i['imagename'])
+    # memory_file.seek(0)
+    return send_file(img, attachment_filename='test.jpg', as_attachment=True)
+    return redirect(url_for('thumb_list', username=username))
 
-@webapp.route('/thumbnails/view/<username>/<i>', methods=['GET'])
-def thumb_view(username,i):
-    # i=int(i)
-    # s3 = boto3.client('s3')
-    # cnx = get_db()
-    # cursor = cnx.cursor()
-    # query = '''
-    #                SELECT images.key1, images.key2, images.key3, images.key4 FROM images
-    #                INNER JOIN users
-    #                ON images.userId = users.id
-    #                WHERE users.id = (SELECT id FROM users WHERE users.login = %s)
-    #             '''
-    # cursor.execute(query, (username,))
-    # images = cursor.fetchall()
-    # urls = []
-    # for j in range(3):
-    #     url = s3.generate_presigned_url(
-    #         ClientMethod='get_object',
-    #         Params={
-    #             'Bucket': 'ece1779project',
-    #             'Key': images[i-1][j+1]
-    #         }
-    #     )
-    #     urls.append(url)
-    return render_template('/thumbnails/view.html', urls=urls)
+    # memory_file = BytesIO()
+    # with zipfile.ZipFile(memory_file, 'w') as zf:
+    #     files = result['files']
+    #     for individualFile in files:
+    #         data = zipfile.ZipInfo(individualFile['fileName'])
+    #         data.date_time = time.localtime(time.time())[:6]
+    #         data.compress_type = zipfile.ZIP_DEFLATED
+    #         zf.writestr(data, individualFile['fileData'])
+    # memory_file.seek(0)
+    # return send_file(memory_file, attachment_filename='capsule.zip', as_attachment=True)
+
+#
+# print 'creating archive'
+# zf = zipfile.ZipFile('zipfile_append.zip', mode='w')
+# try:
+#     zf.write('README.txt')
+# finally:
+#     zf.close()
+#
+# print
+# print_info('zipfile_append.zip')
+#
+# print 'appending to the archive'
+# zf = zipfile.ZipFile('zipfile_append.zip', mode='a')
+# try:
+#     zf.write('README.txt', arcname='README2.txt')
+# finally:
+#     zf.close()
+#
+# print
+# print_info('zipfile_append.zip')
